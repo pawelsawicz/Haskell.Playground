a655844a0a690c72e4a94b88882b403986a5e7d2
pawelsawicz <pawel@sawicz.eu>
2014-11-09 14:53:14 +0100

    finished first exercise, now parseMessage can parse single line of error.log

--- Basics/LogAnalysis.hs
+++ Basics/LogAnalysis.hs
@@ -9,40 +9,23 @@ import Log
 -- Parse sigle line of log for example "E 2 562 help help" -> (Error 2) 562 "help help"
 parseMessage :: String -> LogMessage
 parseMessage [] = Unknown "Empty"
-parseMessage a@(x:_)
-	| isProperFormat(x) = LogMessage (getMessageType(a)) 555 a
-	| otherwise = Unknown a
-
-
-getMessageType :: String -> MessageType
-getMessageType [] = Info -- think how to remove it ?
-getMessageType (x:_) = case x of
-	'I' -> Info
-	'W' -> Warning
-	('E') -> Error 55
-	_ -> Info -- think how to remove it ?
+parseMessage (x:s) = case x of
+	'I' -> LogMessage Info (getTimeStamp(s)) (getRestOfMessage(s))
+	'W' -> LogMessage Warning (getTimeStamp(s)) (getRestOfMessage(s))
+	'E' -> LogMessage (Error (getErrorValue(s))) (getTimeStamp(getRestOfMessage(s))) (getRestOfMessage(getRestOfMessage(s)))
+	_ -> Unknown "Empty"
 
 getTimeStamp :: String -> TimeStamp
 getTimeStamp stringValue = case reads stringValue :: [(Int,String)] of	
 	[(n,_)] -> n
 	_ -> 0
-	
+
+getErrorValue :: String -> Int
+getErrorValue stringValue = case reads stringValue :: [(Int,String)] of	
+	[(n,_)] -> n
+	_ -> 0
+
 getRestOfMessage :: String -> String
 getRestOfMessage stringValue = case reads stringValue :: [(Int,String)] of	
 	[(_,n)] -> n
 	_ -> "Empty"
-
-isProperFormat :: Char -> Bool
-isProperFormat x = case x of
-	'I' -> True
-	'W' -> True
-	'E' -> True
-	_ -> False
-
-parseMess :: String -> LogMessage
-parseMess [] = Unknown "Empty"
-parseMess (x:s) = case x of
-	'I' -> LogMessage Info (getTimeStamp(s)) (getRestOfMessage(s))
-	'W' -> LogMessage Warning (getTimeStamp(s)) (getRestOfMessage(s))
-	'E' -> LogMessage (Error ) 
-	_ -> Unknown "Empty"
